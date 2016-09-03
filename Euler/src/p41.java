bf31c6e2baed032c912cdf903adf081c5a577936
Taylor Henderson <taylor.henderson@telos.com>
2016-09-03 02:15:28 -0400

    p41 answer

--- Euler/src/p41.java
+++ Euler/src/p41.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
-
+//Done
+//7652413
 public class p41 {
 
 	public static void main(String[] args) {
@@ -7,19 +8,20 @@ public class p41 {
 		primes.add(2);
 		int max = 0;
 		boolean found;
-		for(int i = 3; i < 987654321; i+=2){
-			
+		for (int i = 3; i < 987654321; i += 2) {
 			found = false;
-			for(int cur: primes){
-				if(i % cur == 0){
+			for (int cur : primes) {
+				if (i % cur == 0) {
 					found = true;
 					break;
 				}
 			}
-			if(!found){
-				System.out.println(i+"\t"+max);
+			if(!found)
+				primes.add(i);
+			if (!found) {
+				System.out.println(i + "\t" + max);
 			}
-			if(!found && check(""+i)){
+			if (!found && check("" + i)) {
 				max = i;
 			}
 		}
@@ -27,8 +29,8 @@ public class p41 {
 	}
 
 	public static boolean check(String num) {
-		//if (num.length() != 9)
-		//	return false;
+		// if (num.length() != 9)
+		// return false;
 		ArrayList<Character> found = new ArrayList<Character>();
 		while (num.length() > 0) {
 			if (num.charAt(0) == '0')
@@ -38,9 +40,9 @@ public class p41 {
 			found.add(num.charAt(0));
 			num = num.substring(1, num.length());
 		}
-		for(int i = 1; i < found.size()+1; i++){
-			//System.out.println((char)('0'+i));
-			if(found.indexOf((char)('0'+i)) == -1)
+		for (int i = 1; i < found.size() + 1; i++) {
+			// System.out.println((char)('0'+i));
+			if (found.indexOf((char) ('0' + i)) == -1)
 				return false;
 		}
 		return true;
