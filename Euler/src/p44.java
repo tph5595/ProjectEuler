2f15557166156736a27bd478e54020df137191da
Taylor Henderson <tph5595@verizon.net>
2016-09-04 22:46:00 -0400

    p44 update

--- Euler/src/p44.java
+++ Euler/src/p44.java
@@ -4,17 +4,20 @@ public class p44 {
 
 	public static void main(String[] args) {
 		ArrayList<Integer> penNum = new ArrayList<Integer>();
+		int a, b;
 		int bestD = (int) Math.pow(2, 31) - 1;
 		for (int i = 1; i < 1000000; i++) {
 			penNum.add(genNum(i));
 		}
 		for (int i = 1; i < 500000; i++) {
 			for (int j = 0; j < i; j++) {
-				System.out.println(i+"\t"+j);
-				if (penNum.indexOf(penNum.get(i) + penNum.get(j)) != -1 && penNum.indexOf(penNum.get(i) - penNum.get(j)) != -1) {
-					System.out.println(penNum.get(i)+"\t"+penNum.get(j)+"\t"+bestD);
-					if(penNum.get(i)-penNum.get(j) < bestD){
-						bestD = penNum.get(i)-penNum.get(j);
+				a = penNum.get(i);
+				b = penNum.get(j);
+				System.out.println(a + "\t" + b + "\t" + bestD);
+				if (penNum.indexOf(Math.abs(a + b)) != -1 && penNum.indexOf(Math.abs(a - b)) != -1) {
+					System.out.println(a + "\t" + b + "\t" + bestD);
+					if (Math.abs(a - b) < bestD) {
+						bestD = Math.abs(a - b);
 					}
 				}
 			}
