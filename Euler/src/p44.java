3bb43b66099d678bbe90981dbc8522b44ae5dc89
Taylor Henderson <tph5595@verizon.net>
2016-09-05 20:25:26 -0400

    p44 optimized and solution found

--- Euler/src/p44.java
+++ Euler/src/p44.java
@@ -1,22 +1,21 @@
 import java.util.ArrayList;
-
+//Done 5482660
 public class p44 {
-
 	public static void main(String[] args) {
-		ArrayList<Integer> penNum = new ArrayList<Integer>();
-		int a, b;
-		int bestD = (int) Math.pow(2, 31) - 1;
-		for (int i = 1; i < 1000000; i++) {
+		ArrayList<Long> penNum = new ArrayList<Long>();
+		long a, b;
+		long bestD = (long) Math.pow(2, 63);
+		for (int i = 1; i < 100000; i++) {
 			penNum.add(genNum(i));
 		}
-		for (int i = 1; i < 500000; i++) {
+		for (int i = 1; i < 50000; i++) {
 			for (int j = 0; j < i; j++) {
 				a = penNum.get(i);
 				b = penNum.get(j);
-				System.out.println(a + "\t" + b + "\t" + bestD);
-				if (penNum.indexOf(Math.abs(a + b)) != -1 && penNum.indexOf(Math.abs(a - b)) != -1) {
-					System.out.println(a + "\t" + b + "\t" + bestD);
-					if (Math.abs(a - b) < bestD) {
+				System.out.println(i + "\t" + a + "\t" + b + "\t" + bestD);
+				if (Math.abs(a - b) < bestD) {
+					if (penNum.indexOf(Math.abs(a + b)) != -1 && penNum.indexOf(Math.abs(a - b)) != -1) {
+						System.out.println(a + "\t" + b + "\t" + bestD);
 						bestD = Math.abs(a - b);
 					}
 				}
@@ -25,8 +24,7 @@ public class p44 {
 		System.out.println(bestD);
 	}
 
-	public static int genNum(int i) {
-		return i * (3 * i - 1) / 2;
+	public static long genNum(long i) {
+		return i * (3L * i - 1L) / 2L;
 	}
-
 }
