9161a8459ff3a14442cbc8fdc688ce95ecc7d2c0
Taylor Henderson <tph5595@verizon.net>
2016-09-13 13:21:43 -0400

    p50 main method complete

--- Euler/src/p51.java
+++ Euler/src/p51.java
@@ -3,21 +3,57 @@ import java.util.ArrayList;
 public class p51 {
 
 	public static void main(String[] args) {
-		int max = 1000000;
-		ArrayList<Integer> primes   = new ArrayList<Integer>();
-		ArrayList<String>  patterns = new ArrayList<String>();
-		ArrayList<Integer> patNum   = new ArrayList<Integer>();
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		ArrayList<String> patterns = new ArrayList<String>();
+		ArrayList<Integer> patNum = new ArrayList<Integer>();
 		primes.add(2);
-		int maxPat = 0;
+		int maxPat = 0, ndx = 0, curVal;
+		ArrayList<String> strs = null;
 		while (maxPat < 8) {
-			
+			primes.add(nextPrime(primes));
+			if (primes.get(ndx) > 56003) {
+				strs = parse(primes.get(ndx));
+				if (strs != null) {
+					for (int i = 0; i < strs.size(); i++) {
+						if (patterns.indexOf(strs.get(i)) == -1) {
+							patterns.add(strs.get(i));
+							patNum.add(1);
+						} else {
+							curVal = patNum.get(patterns.indexOf(strs.get(i))) + 1;
+							patNum.set(patterns.indexOf(strs.get(i)), curVal);
+							if (curVal > maxPat)
+								maxPat = curVal;
+						}
+					}
+				}
+			}
+			ndx++;
 		}
 
 	}
 
-	public static int nextPrime(ArrayList<Integer>primes, int maxPrime) {
+	private static ArrayList<String> parse(int num) {
+		// split num into array
+		int size = size(num);
+		int[] val = new int[size];
+		for (int i = 0; i < size; i++) {
+			val[size - 1 - i] = num % 10;
+			num /= 10;
+		}
+
+		return null;
+	}
+
+	public static int size(int num) {
+		if (num < 10)
+			return 1;
+		return size(num / 10) + 1;
+	}
+
+	public static int nextPrime(ArrayList<Integer> primes) {
 		boolean found;
-		for (int i = maxPrime+1;; i += 2) {
+		int maxPrime = primes.get(primes.size() - 1);
+		for (int i = maxPrime + 1;; i += 2) {
 			found = false;
 			for (int j = 0; j < primes.size(); j++) {
 				if (i % primes.get(j) == 0) {
@@ -29,7 +65,6 @@ public class p51 {
 				return i;
 			}
 		}
-		//return primes;
 	}
 
 }
